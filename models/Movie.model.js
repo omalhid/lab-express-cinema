22642234b1db59b2f0d8dbe23265e1bb2f1f12c2
Omayma El Hadari <om.elhadari@gmail.com>
2023-12-09 00:00:51 +0100

    Solved lab with no styling

---
+++ models/Movie.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose'); // necessary for creating the schema
+
+const movieSchema= new mongoose.Schema({
+    title: String,
+    director: String,
+    stars: [String],
+    image: String,
+    description: String,
+    showtimes: [String],
+});
+
+// gotta create the model
+const Movie = mongoose.model("Movie", movieSchema);
+
+// since our formule is going to be exported, this is how we indicate it
+module.exports = Movie;
+
